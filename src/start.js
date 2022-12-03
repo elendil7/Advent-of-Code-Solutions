8ff0885c430f9d605948ece2fa1082dcdfbfa5e6
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-03 20:30:35 +0000

    Create main flow with promises & error catching

---
+++ src/start.js
@@ -0,0 +1,19 @@
+const { createStruct } = require('./utils/main/createStruct');
+const { fetchInput } = require('./API/fetchInput');
+
+const run = new Promise(async (res, rej) => {
+	try {
+		// creature directory structure
+		await createStruct();
+		// fetch input from adventofcode.com
+		await fetchInput();
+
+		return res(`All operations sucessfully completed`);
+	} catch (err) {
+		return rej(`\n\nError: ${err}\n\n`);
+	}
+}).then((output) => {
+	console.error(output);
+});
+
+run;
