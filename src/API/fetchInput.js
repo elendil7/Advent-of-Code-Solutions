fb7b5a1309264f56bc411109d92ebd0220e38be3
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-05 03:27:38 +0000

    Error catching + check local storage for input.txt

--- src/API/fetchInput.js
+++ src/API/fetchInput.js
@@ -1,32 +1,54 @@
 require('dotenv').config();
-const { getDay } = require('../utils/misc/getDate');
+const { getDay, getYear } = require('../utils/misc/getDate');
 const { getFullPaths } = require('../utils/main/getFullPaths');
 const { request } = require('undici');
-const { readdirSync, mkdirSync, writeFileSync } = require('fs');
+const { readdirSync, mkdirSync, writeFileSync, readFileSync } = require('fs');
 
 async function fetchInput() {
-	const { statusCode, headers, trailers, body } = await request(
-		`https://adventofcode.com/2022/day/${await getDay()}/input`,
-		{
-			headers: {
-				cookie: `session=${process.env.COOKIE_HASH}`,
-			},
-		}
-	);
+	const day = await getDay();
+	const year = await getYear();
+	const fullPath = (await getFullPaths())[1];
 
-	let text = await body.text();
-	let fullPath = (await getFullPaths())[1];
-
-	// make appropriate directory (if does not exist) & write to appropriate directory
+	// make appropriate directory (if does not exist)
 	try {
 		readdirSync(fullPath);
 	} catch (err) {
 		mkdirSync(fullPath);
 	}
 
-	writeFileSync(`${fullPath}/input.txt`, text);
+	// check local dir if input.txt exists
+	try {
+		const input = readFileSync(`${fullPath}/input.txt`, 'utf-8');
+		return input;
+		// otherwise, fetch input from website
+	} catch (err) {
+		const { statusCode, headers, trailers, body } = await request(
+			`https://adventofcode.com/${year}rgregregre/day/${day}/input`,
+			{
+				headers: {
+					cookie: `session=${process.env.COOKIE_HASH}`,
+				},
+			}
+		);
 
-	return text;
+		// get input / error message
+		const data = await body.text();
+
+		// if success
+		if (statusCode == 200) {
+			//  write to appropriate directory, then return input
+			writeFileSync(`${fullPath}/input.txt`, data);
+			return data;
+		} else {
+			return Error(
+				`\nStatus code: ${statusCode}\nData: ${
+					data.includes('404')
+						? 'No input available yet for day 5! Wait until 05:00AM.\n'
+						: data
+				}`
+			);
+		}
+	}
 }
 
 module.exports = { fetchInput };
