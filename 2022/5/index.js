97fe48b810730f6b32d4909bb1c4f1e1b216370c
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-05 05:31:07 +0000

    2022 Day 5 Solutions

---
+++ 2022/5/index.js
@@ -0,0 +1,103 @@
+// libraries & modules
+const fs = require('fs');
+const { sleep } = require('../../src/utils/misc/sleep');
+const { convert } = require('convert');
+const { fetchInput } = require('../../src/API/fetchInput');
+const {
+	initializeInputExtractionTools,
+} = require('../../src/utils/main/extractInput');
+const { initializeTools, tools } = require('../../src/utils/main/usefulTools');
+
+const start = async () => {
+	initializeTools();
+	initializeInputExtractionTools();
+	const data = await fetchInput();
+	return data instanceof Error ? console.error(data.message) : part1(data);
+};
+start();
+
+// * ATTEMPT #1 (26 minutes to complete both parts!)
+
+// * PART #1
+async function part1(input) {
+	// console.log(input);
+
+	// split input every "n" lines.
+	const split = await input.splitInputEveryNLines(1).slice(10);
+	// console.log('SplitEveryNLines: ', split);
+
+	// * Write code here:
+
+	let key = {
+		1: ['F', 'G', 'V', 'R', 'J', 'L', 'D'],
+		2: ['S', 'J', 'H', 'V', 'B', 'M', 'P', 'T'],
+		3: ['C', 'P', 'G', 'D', 'F', 'M', 'H', 'V'],
+		4: ['Q', 'G', 'N', 'P', 'D', 'M'],
+		5: ['F', 'N', 'H', 'L', 'J'],
+		6: ['Z', 'T', 'G', 'D', 'Q', 'V', 'F', 'N'],
+		7: ['L', 'B', 'D', 'F'],
+		8: ['N', 'D', 'V', 'S', 'B', 'J', 'M'],
+		9: ['D', 'L', 'G'],
+	};
+
+	for (let i = 0; i < split.length; ++i) {
+		const cur = String(split[i]).split(' ');
+		const howManyCrates = cur[1];
+		const from = cur[3];
+		const to = cur[5];
+
+		for (let x = 0; x < howManyCrates; ++x) {
+			key[to].unshift(key[from].shift());
+		}
+
+		// console.log(cur);
+	}
+
+	let res = '';
+
+	for (let v in key) {
+		res += key[v][0];
+	}
+
+	console.log(res);
+
+	part2(input, split);
+}
+
+// * PART #2
+async function part2(input, split) {
+	let key = {
+		1: ['F', 'G', 'V', 'R', 'J', 'L', 'D'],
+		2: ['S', 'J', 'H', 'V', 'B', 'M', 'P', 'T'],
+		3: ['C', 'P', 'G', 'D', 'F', 'M', 'H', 'V'],
+		4: ['Q', 'G', 'N', 'P', 'D', 'M'],
+		5: ['F', 'N', 'H', 'L', 'J'],
+		6: ['Z', 'T', 'G', 'D', 'Q', 'V', 'F', 'N'],
+		7: ['L', 'B', 'D', 'F'],
+		8: ['N', 'D', 'V', 'S', 'B', 'J', 'M'],
+		9: ['D', 'L', 'G'],
+	};
+
+	for (let i = 0; i < split.length; ++i) {
+		const cur = String(split[i]).split(' ');
+		const howManyCrates = cur[1];
+		const from = cur[3];
+		const to = cur[5];
+
+		key[to] = key[from].slice(0, howManyCrates).concat(key[to]);
+		key[from] = key[from].slice(howManyCrates);
+
+		//console.log(cur);
+		//console.log(key);
+	}
+
+	let res = '';
+
+	//console.log(key);
+
+	for (let v in key) {
+		res += key[v][0];
+	}
+
+	console.log(res);
+}
