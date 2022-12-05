e3b07e3213293bef7e6ac64a8cfadeeb1ba7faf7
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-05 03:28:05 +0000

    Changed to global functions for swifter execution

--- src/utils/main/extractInput.js
+++ src/utils/main/extractInput.js
@@ -1,47 +1,48 @@
-async function splitInputEveryNLines(input, lines) {
-	// split by each line
-	const arr = input.toString().trim().split('\n');
+const initializeInputExtractionTools = () => {
+	String.prototype.splitInputEveryNLines = function (lines) {
+		// split by each line
+		const arr = this.toString().trim().split('\n');
 
-	// edge cases (for human error)
-	if (lines === 0) return input;
-	// clean lines input
-	if (lines < 1) lines = Math.abs(lines);
+		// edge cases (for human error)
+		if (lines === 0) return this;
+		// clean lines input
+		if (lines < 1) lines = Math.abs(lines);
 
-	// define an output and a temporary array respectively.
-	let clusters = [];
-	let temp = [];
+		// define an output and a temporary array respectively.
+		let clusters = [];
+		let temp = [];
 
-	// bundle up multiple lines into arrays based on the number the user passed in (split at every n lines).
-	for (let i = 0; i < arr.length; ++i) {
-		const cur = String(arr[i]).replace(/(\r\n|\n|\r)/gm, '');
-		temp.push(cur);
-		if ((i + 1) % lines == 0) {
-			clusters.push(temp);
-			temp = [];
-		}
-	}
-
-	// return bundled up lines as 2D array
-	return clusters;
-}
-
-async function splitInputEveryBlankLine(input) {
-	const arr = input.toString().trim().split('\n');
-
-	let clusters = [];
-	let temp = [];
-
-	for (let i = 0; i <= arr.length; ++i) {
-		const cur = String(arr[i]).replace(/(\r\n|\n|\r)/gm, '');
-		if (cur == '' || (!cur && temp.length > 0)) {
-			clusters.push(temp);
-			temp = [];
-		} else {
+		// bundle up multiple lines into arrays based on the number the user passed in (split at every n lines).
+		for (let i = 0; i < arr.length; ++i) {
+			const cur = String(arr[i]).replace(/(\r\n|\n|\r)/gm, '');
 			temp.push(cur);
+			if ((i + 1) % lines == 0) {
+				clusters.push(temp);
+				temp = [];
+			}
 		}
-	}
 
-	return clusters;
-}
+		// return bundled up lines as 2D array
+		return clusters;
+	};
+	String.prototype.splitInputEveryBlankLine = function () {
+		const arr = this.toString().trim().split('\n');
 
-module.exports = { splitInputEveryNLines, splitInputEveryBlankLine };
+		let clusters = [];
+		let temp = [];
+
+		for (let i = 0; i <= arr.length; ++i) {
+			const cur = String(arr[i]).replace(/(\r\n|\n|\r)/gm, '');
+			if (cur == '' || (!cur && temp.length > 0)) {
+				clusters.push(temp);
+				temp = [];
+			} else {
+				temp.push(cur);
+			}
+		}
+
+		return clusters;
+	};
+};
+
+module.exports = { initializeInputExtractionTools };
