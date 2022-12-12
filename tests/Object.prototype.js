f94a5b80f3641c07cfe4969fc8fe4a541b7aa189
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-12 19:58:38 +0000

    Added more test cases (javascript snippets)

--- tests/Object.prototype.js
+++ tests/Object.prototype.js
@@ -8,19 +8,37 @@ start();
 
 function test() {
 	let firstObject = {
-		Jack: 29,
-		David: 13,
-		William: 100,
-		Spongebob: 390,
-		Dog: 7,
-		Cat: 19,
-		Tiger: 1,
-		Wolf: 60,
-		Lion: 210,
+		Jack: [3, 56, 7],
+		David: [13],
+		William: [100],
+		Spongebob: [390],
+		Dog: [7],
+		Cat: [19],
+		Tiger: [1],
+		Wolf: [60],
+		Lion: [210],
 	};
+	let secondObj = { 4: 'strength' };
 
 	console.log(firstObject.sortLowToHigh());
 	console.log(firstObject.sortHighToLow());
 	console.log(firstObject.sortByValues());
 	console.log(tools.populateArray(100, 130));
+
+	for (let key in firstObject) {
+		if (!firstObject.hasOwnProperty(key)) continue;
+
+		const nums = firstObject[key];
+
+		console.log(`\nKey: `, key);
+		console.log(`Nums: `, nums);
+
+		for (let i = 0; i < nums.length; i++) {
+			console.log(nums[i]);
+		}
+	}
+
+	for (const [key, val] of Object.entries(firstObject)) {
+		console.log(key, val);
+	}
 }
