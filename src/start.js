8a648cd1ccf11758a916d77655082bf4233f4086
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-16 17:59:22 +0000

    Add comments for future reference

--- src/start.js
+++ src/start.js
@@ -3,9 +3,9 @@ const { fetchInput } = require('./API/fetchInput');
 
 const run = new Promise(async (res, rej) => {
 	try {
-		// creature directory structure
+		// creature directory structure (day, year) (no args = current date)
 		await createStruct();
-		// fetch input from adventofcode.com
+		// fetch input from adventofcode.com (day year) (no args = current date)
 		await fetchInput();
 
 		return res(`All operations sucessfully completed\n`);
