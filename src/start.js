a54798f67201bd458677ed19968a6c1b412009ca
elendil1973 <80429382+elendil7@users.noreply.github.com>
2023-02-12 20:14:57 +0000

    Ignore

--- src/start.js
+++ src/start.js
@@ -4,9 +4,9 @@ const { fetchInput } = require('./API/fetchInput');
 const run = new Promise(async (res, rej) => {
 	try {
 		// creature directory structure
-		await createStruct();
+		await createStruct(3, 2015);
 		// fetch input from adventofcode.com
-		await fetchInput();
+		await fetchInput(3, 2015);
 
 		return res(`All operations sucessfully completed\n`);
 	} catch (err) {
